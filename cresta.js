63cab78a2a76535f61d69b7191addd36d904e198
19yuki99 <88812862+19yuki99@users.noreply.github.com>
2021-08-14 22:24:35 +0900

    Add files via upload

---
+++ cresta.js
@@ -0,0 +1,34 @@
+//sticky header
+$(function(){
+  var $win = $(window),
+      $fv = $('.fv'),
+      $header = $('.header'),
+      fvHeight = $fv.outerHeight();
+      fixedClass = 'fixed';
+    $win.on('load scroll', function(){
+      var value = $(this).scrollTop();
+      if($win. width() > 768){
+        if(value > fvHeight){
+         $header.addClass(fixedClass);
+        }else{
+          $header.removeClass(fixedClass);
+        } 
+      }
+    });
+});
+
+//slider
+$('.slider').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+//hamburger menu
+$('.burger-btn').on('click', function(){
+  $('.nav-wrapper').fadeToggle(300);
+  $(this).toggleClass('cross')
+});
